d997bdbe31cbac618bc65186979f8d468bad3a91
AndrewOPP <a.vyblov2003@gmail.com>
2026-04-17 13:54:54 +0300

    fix: some minor fixes

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@time-tracking-app/database';
 
-// const FRONTEND_URL = 'https://viso-web.onrender.com';
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 @Injectable()
